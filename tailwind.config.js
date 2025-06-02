8b379aa9a7f16da3eefe8cc2a93bd19b5a76e3c9
patricklinzzz <p9011910pat@gmail.com>
2025-06-03 03:29:41 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'brand-text': '#4D4012',
+        'brand-pink': '#E8BEA4',
+        'brand-green': '#DCDEA5'
+      },
+    },
+  },
+  plugins: [],
+}
+
