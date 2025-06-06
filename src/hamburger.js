72575c223a4f0aeb2127deca7021b0969eb5fd84
patricklinzzz <p9011910pat@gmail.com>
2025-06-06 11:08:45 +0800

    1.4.4 scrolltop

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,25 +1,62 @@
-const menuButton = document.getElementById('menuButton');
-const closeMenuButton = document.getElementById('closeMenuButton');
-const mobileMenu = document.getElementById('mobileMenu');
+const menuButton = document.getElementById('menuButton')
+const closeMenuButton = document.getElementById('closeMenuButton')
+const mobileMenu = document.getElementById('mobileMenu')
 
 menuButton.addEventListener('click', () => {
-    mobileMenu.classList.remove('hidden');
-    mobileMenu.classList.remove('translate-x-full');
-    mobileMenu.classList.add('translate-x-0');
-});
+    mobileMenu.style.display='block'
+    setTimeout(() => {
+        mobileMenu.style.transform='translateX(0)'
+    }, 50)
+})
 
 closeMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.add('translate-x-full');
-    mobileMenu.classList.remove('translate-x-0');
+    mobileMenu.style.transform='translateX(100%)'
     setTimeout(() => {
-        mobileMenu.classList.add('hidden');
-    }, 300);
-});
+        mobileMenu.style.display='hidden'
+    }, 200)
+})
 document.addEventListener('click', function(event) {
-    const isClickInsideMenu = mobileMenu.contains(event.target);
-    const isClickOnMenuButton = menuButton.contains(event.target);
+    const isClickInsideMenu = mobileMenu.contains(event.target)
+    const isClickOnMenuButton = menuButton.contains(event.target)
 
     if (!isClickInsideMenu && !isClickOnMenuButton && !mobileMenu.classList.contains('hidden')) {
-        closeMenuButton.click();
+        closeMenuButton.click()
     }
-});
+})
+// 固定導覽列
+// 回到上方
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollTopBtn = document.getElementById("scrollTopBtn")
+    window.onscroll = function() {scrollFunction()}
+    //header動畫
+    const header = document.querySelector('header')
+    let preScroll = 0
+
+    function scrollFunction() {
+        if (document.documentElement.scrollTop > 80) {
+        scrollTopBtn.style.display = "block"
+        } else {
+        scrollTopBtn.style.display = "none"
+        }
+
+        const nowScroll =document.documentElement.scrollTop
+        if(document.documentElement.scrollTop>20){
+            header.style.backgroundColor=''
+            if(nowScroll<preScroll){
+                header.style.transform="translateY(0%)" //往上
+            }else if(nowScroll>preScroll){
+                header.style.transform="translateY(-100%)" //往下
+            }
+        preScroll=nowScroll
+        }
+        else{
+            header.style.transform="translateY(0%)"
+        }
+    }
+    scrollTopBtn.addEventListener('click', function() {
+        window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+        })
+    })
+})
